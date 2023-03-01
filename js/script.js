7b18b28288c92aa1be471d1d0b8b8700c6b7d111
PLuIIIkin <viazovskih@rambler.ru>
2023-03-01 20:58:43 +0800

    start

--- js/script.js
+++ js/script.js
@@ -204,33 +204,50 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  new MenuCard(
-    'img/tabs/vegy.jpg',
-    'vegy',
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container',
-    'menu__item'
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url)
 
-  new MenuCard(
-    'img/tabs/elite.jpg',
-    'elite',
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    15,
-    '.menu .container'
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status:${res.status}`);
+    }
 
-  new MenuCard(
-    'img/tabs/post.jpg',
-    'post',
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    12,
-    '.menu .container'
-  ).render();
+    return await res.json();
+ }
+
+ getResource('http://localhost:3000/menu')
+ .then(data => {
+   data.forEach(({img, altimg, title, descr, price}) => {
+     new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+   });
+ });
+
+  // new MenuCard(
+  //   'img/tabs/vegy.jpg',
+  //   'vegy',
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   9,
+  //   '.menu .container',
+  //   'menu__item'
+  // ).render();
+
+  // new MenuCard(
+  //   'img/tabs/elite.jpg',
+  //   'elite',
+  //   'Меню “Премиум”',
+  //   'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+  //   19,
+  //   '.menu .container'
+  // ).render();
+
+  // new MenuCard(
+  //   'img/tabs/post.jpg',
+  //   'post',
+  //   'Меню "Постное"',
+  //   'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+  //   12,
+  //   '.menu .container'
+  // ).render();
 
   // Forms
 
@@ -289,10 +306,22 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+     const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: data
+     })
+
+     return await res.json();
+  }
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -305,32 +334,24 @@ window.addEventListener('DOMContentLoaded', () => {
       
       form.insertAdjacentElement('afterend', statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-
-      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
       const object = {};
 
-      formData.forEach(function(val, key) {
-        object[key] = val;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      const json = JSON.stringify(object)
-
-      request.send(json);
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          console.log(request.response);
-          showThanksModal(messaga.success);
-          form.reset();
-          statusMessage.remove();
-        } else {
-          showThanksModal(messaga.failure);
-        }
-      });
+      
+      postData('http://localhost:3000/requests', json)
+      .then(data => {
+        console.log(data);
+        showThanksModal(messaga.success);
+        form.reset();
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(messaga.failure);
+      }).finally(() => {
+        form.reset();
+      })
     });
   }
 
@@ -359,4 +380,6 @@ window.addEventListener('DOMContentLoaded', () => {
       modalClose();
     }, 4000)
   }
+
+ 
 });
